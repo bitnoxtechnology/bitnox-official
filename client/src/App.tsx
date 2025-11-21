daa7f8c169cf06146227d4b3299a2ac48dad04b3
Faleye Oluwafemi Joseph <faleyeoluwafemi1@gmail.com>
2025-11-21 23:22:09 +0100

    update import path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Herosection from "./components/Herosection";
-import About from "./components/About";
-import WhyUs from "./components/WhyUs";
-import Portfolio from "./components/Portfolio";
-import Testimonial from "./components/Testimonial";
-import Conclusion from "./components/Conclusion";
-import Footer from "./components/Footer";
-import AboutUs from "./pages/AboutUs";
-import Cleaning from "./pages/Cleaning";
-import Contact from "./pages/Contact";
-import Meta from "./components/Meta";
+import Navbar from "@/components/Navbar";
+import Herosection from "@/components/Herosection";
+import About from "@/components/About";
+import WhyUs from "@/components/WhyUs";
+import Portfolio from "@/components/Portfolio";
+import Testimonial from "@/components/Testimonial";
+import Conclusion from "@/components/Conclusion";
+import Footer from "@/components/Footer";
+import AboutUs from "@/pages/AboutUs";
+import Cleaning from "@/pages/Cleaning";
+import Contact from "@/pages/Contact";
+import Meta from "@/components/Meta";
 import { Toaster } from "sonner";
 
 function App() {
